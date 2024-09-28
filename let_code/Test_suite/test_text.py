38b1c962b4b93578469a12ec7a7b8e7c488d0070
vinuth reddy <reddyvinuth27@gmail.com>
2024-09-28 21:01:25 +0530

    change 2

--- let_code/Test_suite/test_text.py
+++ let_code/Test_suite/test_text.py
@@ -4,7 +4,7 @@ from let_code.POM.text_page import Text
 def test_text(driver):
     text_page_obj=Text(driver)
     text_page_obj.text_("vinuth",
-                        "i am too  good what about you baby!....",
+                        "i am too  good what about you....",
                         "heyy .....",
                         "......",
                         "vinuth",
